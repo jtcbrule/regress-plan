09b5c317b278660d631d1d1254f41aa40f3d22cf
jtcb <jtcbrule@gmail.com>
2013-10-16 14:51:36 -0400

    function fitting now works with scipy/sympy

--- fit.py
+++ fit.py
@@ -1,17 +1,19 @@
 #!/bin/env python3
-from sympy import symbols, Lambda, lambdify
+from sympy import symbols, lambdify
 from scipy.optimize import curve_fit
-import numpy as np
+import numpy
+
+# Notes: curve_fit needs numpy arrays; numpy arrays are typed, so make sure at least one element of the array is a float to upcast
 
 # maximum number of constants in a symbolic expression
 MAX_CONSTANTS = 10
 
-# symbolical constants (note that c[0] = x)
+# symbolical constants/variable (note that c[0] = x)
 x = symbols('x')
 c = list(symbols('c1:' + str(MAX_CONSTANTS)))
 c.insert(0, x)
 
-def to_function(expr, num_param):
+def to_lambda(expr, num_param):
     """ Return a (python) lambda expression 
 
         expr - symbolic expression composed of x and c1...ck
@@ -19,18 +21,18 @@ def to_function(expr, num_param):
         
         Note that expr cannot contain constant ck for k > num_param
     """
-    return Lambda(c[:(num_param + 1)], expr)
+    return lambdify(c[:(num_param + 1)], expr)
 
 
 sample_expr = c[1] * x**2 + c[2]
 
 #2 * x^2 + 1
-x_data = np.array([0.0, 1, 2, 3, 4, 5]) #need float dtype
-y_data = np.array([1.0, 3, 9, 19, 33, 51]) #exact
-y_err = np.array([1.1, 3.1, 8.9, 19.8, 33.1, 50]) #with some error
-guess = np.array([1.0, 1.0]) #guess needed since scipy can't infer #of params from sympy.Lambda
+x_data = numpy.array([0, 1, 2, 3, 4, 5])
+y_data = numpy.array([1, 3, 9, 19, 33, 51]) #exact
+y_err = numpy.array([1.1, 3.1, 8.9, 19.8, 33.1, 50]) #with some error
+guess = numpy.array([1.0, 1.0]) 
 
-f = to_function(sample_expr, 2)
+f = to_lambda(sample_expr, 2)
 
 #popt, pcov = curve_fit(f, x_data, y_data)
 #print("Fit to y_data")
