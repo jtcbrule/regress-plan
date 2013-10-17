c89ab2dc7e6c637445d6c950702228380e5d0019
jtcb <jtcbrule@gmail.com>
2013-10-16 23:04:29 -0400

    curve_fit() works

--- fit.py
+++ fit.py
@@ -1,7 +1,7 @@
 #!/bin/env python3
 from sympy import symbols, lambdify
 from scipy.optimize import curve_fit
-import numpy
+import numpy as np
 
 # maximum number of constants in a symbolic expression
 MAX_CONSTANTS = 10
@@ -24,19 +24,22 @@ def to_lambda(expr, num_param):
 
 sample_expr = c[1] * x**2 + c[2]
 
-#2 * x^2 + 1
-x_data = numpy.array([0, 1, 2, 3, 4, 5])
-y_data = numpy.array([1, 3, 9, 19, 33, 51]) #exact
-y_err = numpy.array([1.1, 3.1, 8.9, 19.8, 33.1, 50]) #with some error
-guess = numpy.array([1.0, 1.0]) 
+# 2 * x^2 + 1
+x_data = np.array([0, 1, 2, 3, 4, 5])
+y_data = np.array([1, 3, 9, 19, 33, 51]) #exact
+y_err = np.array([1.1, 3.1, 8.9, 19.8, 33.1, 50]) #with some error
+guess = np.array([1.0, 1.0]) #optional
 
 f = to_lambda(sample_expr, 2)
 
-#popt, pcov = curve_fit(f, x_data, y_data)
-#print("Fit to y_data")
-#print(popt)
-#print(pcov)
+# note that trace(cov) is equal to mse for unbiased estimators
+# also, note that curve_fit requires numpy arrays
+popt, pcov = curve_fit(f, x_data, y_data)
+print("Fit to y_data")
+print(popt)
+print(np.sum(np.diag(pcov)))
 
-#popt, pcov = curve_fit(f, x_data, y_err)
-#print("Fit to y_err")
-#print(popt)
+popt, pcov = curve_fit(f, x_data, y_err)
+print("Fit to y_err")
+print(popt)
+print(np.sum(np.diag(pcov)))
