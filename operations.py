3da79b72fc2e875e277870dde638377d4c7181b5
jtcb <jtcbrule@gmail.com>
2013-11-20 18:52:38 -0500

    Changed operations.py to use unary_minus() instead of sub()

--- operations.py
+++ operations.py
@@ -17,30 +17,29 @@ tan = sympy.tan
 exp = sympy.exp
 atan = sympy.atan
 
-unary_ops = [sqrt, log, sin, tan, exp, atan]
-# consider using unary minus instead of sub?
+def unary_minus(x):
+    return -1 * x
+
+unary_minus.__name__ = '-'
+
+unary_ops = [sqrt, log, sin, tan, exp, atan, unary_minus]
 
 ''' Binary: '''
 def add(x, y):
     return x + y
-def sub(x, y):
-    return x - y
 def mul(x, y):
     return x * y
 def div(x, y):
     return x / y
 
 add.__name__ = '+'
-sub.__name__ = '-'
 mul.__name__ = '*'
 div.__name__ = '/'
 
-binary_ops = [add, sub, mul, div]
+binary_ops = [add, mul, div]
 
 def get_random_unary_op():
     return random.choice(unary_ops)
 
 def get_random_binary_op():
     return random.choice(binary_ops)
-
-
