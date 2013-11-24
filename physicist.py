3fdff211571b25e6ead8c2b19d9f68e37ebc3ca3
jtcb <jtcbrule@gmail.com>
2013-11-24 13:46:34 -0500

    Worked on physicist.py

--- physicist.py
+++ physicist.py
@@ -1,7 +1,7 @@
 ''' "Physicist" search
 '''
 
-import numpy as np
+import numpy 
 import sympy
 
 import operations as ops
@@ -10,10 +10,11 @@ import fit
 import exptree
 import random
 
-x, j, k = sympy.symbols('x')
+''' Convention: j, k are constants for prototypes'''
+x, c, j, k = sympy.symbols('x c j k')
 
 '''List of function prototypes and their constants'''
-prototypes = [ops.sqrt(x + j), ops.log(x + j), ops.sin(i*), ops.exp(x*j)]
+prototypes = [ops.sqrt(x + j), ops.log(x + j), ops.exp(x*j)]
 
 
 def cross_multiply(x, y):
@@ -23,19 +24,33 @@ def cross_multiply(x, y):
     return [i * j for i in x for j in y if i != j]
 
 
-def physics_fit(prototypes, threshold, max_terms, epsilon=0, mul_depth=1):
+def physics_fit(prototypes, x_data, y_data, threshold=1, max_terms=None,
+                fit_tries=3, epsilon=0, mul_depth=1):
     '''Do a "physicist fit"
     prototypes - list of base functions
-    threshold - stop when fit error is below this
-    max_terms - maximum number of base function terms
+    x_data, y_data - data to fit
+    threshold - stop when fit error is below threshold
+    max_terms - stop when model has this many potototype functions
     epsilon - probability of choosing non-optimal term
-    mul-depth - generates 
+    mul-depth - generates more prototypes by cross_multipying
     '''
 
     # TODO: cross_multiply mul_depth times
+   
+    # calculate error levels for every function prototype
+    error_level = []
+    for func in prototypes:
+        # add a leading constant for better fitting
+        func_plus_const = func + c
+        constants = func_plus_const.free_symbols 
+        constants.remove(x) 
+        constants = list(constants)
 
+        opt, err = fit.sym_fit(func_plus_const, constants, x_data, y_data, fit_tries)
+        if err < numpy.inf:
+            error_level.append((err, func))
     
-
+    return error_level
 
 def main():
     pass
