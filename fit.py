677484ea8a15df31375526237b16bd6ad2a26049
jtcb <jtcbrule@gmail.com>
2013-11-25 00:53:44 -0500

    Bug fix for fit.sym_fit(); more work on physics_fit()

--- fit.py
+++ fit.py
@@ -27,15 +27,18 @@ def sse(f, x_data, y_data):
     ''' 
     return ((f(x_data) - y_data)**2).sum()
     
-def sym_fit(expr, constants, x_vals, y_vals, guesses=None):
+def sym_fit(expr, constants, x_vals, y_vals, guesses=None, max_iter=0):
     ''' Fit sympy expression expr of x and constants to vals.
     Return (optimal_fit, sum_squares_error)
     If no fit can be found, sse will be either a numpy inf or nan.
     
     guesses>0 will perform multiple curve_fits with different
-    initial values drawn from (N(0, abs(x_data).max() * 1.5) and return the best.
-    (This gives approximately a 50% chance the param will be within the range
-     of the x vals and 50% the param will be outside the range of x_vals)
+    initial values drawn from N(x_data.mean(), range(x_data)))
+    (This gives approximately a 2/3 chance the param will be within the range 
+    of the x vals and 1/3 the param will be outside the range of x_vals)
+    
+    max_iter is the maximum number of iterations the LM algorithm runs.
+    max_iter = 0 will use scipy.optimize.curve_fit() defaults.
     '''
     f = to_lambda(expr, constants)
     x_data = numpy.array(x_vals)
@@ -45,25 +48,38 @@ def sym_fit(expr, constants, x_vals, y_vals, guesses=None):
     if len(constants) == 0:
         return ([], sse(f, x_data, y_data))
 
-    # no guesses: 1 for all parameters, fit and return
-    if not guesses:
-        popt, pcov = scipy.optimize.curve_fit(f, x_data, y_data)
-        f_opt = lambda x: f(x, *popt)
-        err = sse(f_opt, x_data, y_data)
-        return (popt, err)
-    
-    # multiple guesses
+    # fit with all ones guess
     min_err = numpy.inf
     min_popt = numpy.ones(len(constants))
-    for i in range(0, guesses):
-        # run a fit with a guess
-        guess = numpy.random.randn(len(constants)) * numpy.abs(x_data).max() * 1.5
-        popt, pcov = scipy.optimize.curve_fit(f, x_data, y_data, guess)
+    
+    try:
+        popt, pcov = scipy.optimize.curve_fit(f, x_data, y_data, maxfev=max_iter)
         f_opt = lambda x: f(x, *popt)
         err = sse(f_opt, x_data, y_data)
-        
         if err < min_err:
             min_err = err
             min_popt = popt
+    except:
+        pass
+    
+    if not guesses:
+        return (min_popt, min_err)
+    
+    # run fits with initial guess
+    x_mean = x_data.mean()
+    x_range = x_data.max() - x_data.min()
+    for i in range(0, guesses):
+        # generate guess
+        guess = numpy.random.randn(len(constants)) * x_range + x_mean
+        
+        try:
+            popt, pcov = scipy.optimize.curve_fit(f, x_data, y_data, guess, maxfev=max_iter)
+            f_opt = lambda x: f(x, *popt)
+            err = sse(f_opt, x_data, y_data)
+            if err < min_err:
+                min_err = err
+                min_popt = popt
+        except:
+            pass
             
     return (min_popt, min_err)
--- physicist.py
+++ physicist.py
@@ -1,3 +1,5 @@
+#!/bin/env python3
+
 ''' "Physicist" search
 '''
 
@@ -10,11 +12,13 @@ import fit
 import exptree
 import random
 
+x = sympy.symbols('x')
+
 ''' Convention: j, k are constants for prototypes'''
-x, c, j, k = sympy.symbols('x c j k')
+j, k = sympy.symbols('j k')
 
 '''List of function prototypes and their constants'''
-prototypes = [ops.sqrt(x + j), ops.log(x + j), ops.exp(x*j)]
+basic_functions = [x, x**2, ops.sqrt(x + j), ops.log(x + j), ops.exp(x*j), ops.sin(x*j +k)]
 
 
 def cross_multiply(x, y):
@@ -23,34 +27,75 @@ def cross_multiply(x, y):
     '''
     return [i * j for i in x for j in y if i != j]
 
+def rename_constants(prototype, cur_constants):
+    '''Rename the constants in prototype expression to avoid name collisions.
+    Returns renamed prototype; cur_constants is updated in-place.
+    '''
+    prototype_constants = list(prototype.free_symbols)
+    prototype_constants.remove(x)
+
+    j = len(cur_constants)
+    k = len(prototype_constants)
+    new_constants = [sympy.symbols('c' + str(i)) for i in
+                     range(j, j + k)]
+    
+    cur_constants.extend(new_constants)
+    return prototype.subs(zip(prototype_constants, new_constants))
 
 def physics_fit(prototypes, x_data, y_data, threshold=1, max_terms=None,
-                fit_tries=3, epsilon=0, mul_depth=1):
+                fit_tries=3, mul_depth=1, epsilon=0):
     '''Do a "physicist fit"
     prototypes - list of base functions
     x_data, y_data - data to fit
     threshold - stop when fit error is below threshold
     max_terms - stop when model has this many potototype functions
-    epsilon - probability of choosing non-optimal term
     mul-depth - generates more prototypes by cross_multipying
+    epsilon - probability of choosing non-optimal term
     '''
 
     # TODO: cross_multiply mul_depth times
+
+    # zero-order model
+    c0 = sympy.symbols('c0')
    
     # calculate error levels for every function prototype
-    error_level = []
+    base_score = []
     for func in prototypes:
         # add a leading constant for better fitting
-        func_plus_const = func + c
+        func_plus_const = func + c0
         constants = func_plus_const.free_symbols 
         constants.remove(x) 
         constants = list(constants)
 
-        opt, err = fit.sym_fit(func_plus_const, constants, x_data, y_data, fit_tries)
+        opt, err = fit.sym_fit(func_plus_const, constants, x_data, y_data,
+                               guesses=fit_tries)
+
         if err < numpy.inf:
-            error_level.append((err, func))
+            base_score.append((err, func))
     
-    return error_level
+    base_score = sorted(base_score, key=lambda x: x[0], reverse=True)
+    
+    model_constants = [c0]
+    model = c0
+    opt, cur_err = fit.sym_fit(model, model_constants, x_data, y_data)
+
+    print("Baseline error:", cur_err) #TODO remove
+     
+    if max_terms==None: max_terms = len(prototypes) 
+
+    for i in range(0, max_terms):
+        if cur_err < threshold: break
+        
+        # update model with best scoring prototype and re-fit
+        best_expr = base_score.pop()[1]
+        model += rename_constants(best_expr, model_constants)
+        opt, cur_err = fit.sym_fit(model, model_constants, x_data, y_data,
+                                   fit_tries)
+
+        # TODO remove
+        print("Current Model:", model, "Error:", cur_err)
+
+    return (model, opt, cur_err)
 
 def main():
     pass
